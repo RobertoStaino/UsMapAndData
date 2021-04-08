2cd81c37cf125d676a0fd8f6072ae6c5f667116b
RobertoStaino <80687314+RobertoStaino@users.noreply.github.com>
2021-04-08 18:57:21 +0200

    Update helpers.R

--- helpers.R
+++ helpers.R
@@ -1,4 +1,4 @@
-state<-as_tibble(map_data("state")) 
+state <- as_tibble(map_data("state")) 
 state <- state[state$region != "district of columbia",]
 custom_map <- function (data, var, name, color){
   if(name == "Party share" || name == "Mid Term Election") {
